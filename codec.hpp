7ff296717fedaf0a7c6a9e353f668004b8deb23b
Nick Hurst <nick@hurstsolutions.io>
2025-07-13 00:42:52 -0400

    Separated common declarations in prep for decoder.

---
+++ codec.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <vector>
+
+constexpr int BLOCK_MATRIX_SIZE = 64;
+
+struct Image {
+    int width;
+    int height;
+    std::vector<unsigned char> data;
+};
+
+struct YCbCrImage{
+    int width;
+    int height;
+    std::vector<unsigned char> y_data;
+    std::vector<unsigned char> cb_data;
+    std::vector<unsigned char> cr_data;
+};
+
+struct Block{
+    std::vector<double> data;
+    Block() : data(BLOCK_MATRIX_SIZE, 0.0){}
+};
+
+extern const int LUMINANCE_QUANT_TABLE[BLOCK_MATRIX_SIZE];
+extern const int ZIG_ZAG_INDEX_ORDER[BLOCK_MATRIX_SIZE];
